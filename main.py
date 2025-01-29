dc44d9aa03d7ece1d678e9471243917a19c51cd8
ch0ppah <nick0suna76@gmail.com>
2025-01-28 22:33:51 -0500

    add count_chars function

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+        count_words()
+        count_chars()
+
+def count_words():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        word_count = len(file_contents.split())
+        print(word_count)
+
+def count_chars():
+    chars = {}
+
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read().lower()
+        
+        for char in file_contents:
+            chars.setdefault(char, 0)
+            chars[char] += 1
+    
+    print(chars)
+main()
